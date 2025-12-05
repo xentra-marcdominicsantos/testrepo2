56ba47b845d29db363cd850fbb3fad06b2c860ce
xentra-marcdominicsantos <marcdominic.santos@xentragroup.com>
2025-12-05 21:31:22 +0800

    Update program.cs

--- service-a/program.cs
+++ service-a/program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "This is another try");
+app.MapGet("/", () => "This is another fckin try");
 
 app.Run();
